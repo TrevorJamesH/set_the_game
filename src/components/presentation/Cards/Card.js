532bdef359caa4e695fa5c1bcb1b8fc313ec53b5
mikeadossi <mikeadossi@gmail.com>
2017-04-03 17:24:57 -0700

    create game shapes

--- src/components/presentation/Cards/Card.js
+++ src/components/presentation/Cards/Card.js
@@ -2,10 +2,50 @@ import React from 'react';
 
 export default class Card extends React.Component{
 
+  // <div className="leftDiamond"></div>
+  // <div className="rightDiamond"></div>
+  // </div>
+  // <div className="card diamond">
+  // <div className="leftDiamond"></div>
+  // <div className="rightDiamond"></div>
+  // </div>
+  // <div className="card diamond">
+  // <div className="leftDiamond"></div>
+  // <div className="rightDiamond"></div>
+
+  // <div className="card item1">
+  // <div className="oval"></div>
+  // </div>
+  // <div className="card item2">
+  // <div className="oval"></div>
+  // </div>
+  // <div className="card item3">
+  // <div className="oval"></div>
+  // </div>
+
+  // <div className="card">
+  // <div className="squiggly"></div>
+  // </div>
+  // <div className="card">
+  // <div className="squiggly"></div>
+  // </div>
+  // <div className="card">
+  // <div className="squiggly"></div>
+  // </div>
+
   render(){
     return(
-      <div id="Card">
-        card
+      <div>
+        <div className="card diamond">
+          <div className="leftDiamond"></div>
+          <div className="rightDiamond"></div>
+        </div>
+        <div className="card item3">
+          <div className="oval"></div>
+        </div>
+        <div className="card">
+          <div className="squiggly"></div>
+        </div>
       </div>
     );
   };
--- src/components/presentation/Cards/index.js
+++ src/components/presentation/Cards/index.js
@@ -1,3 +1,5 @@
+
+
 var cards = [
   {
     id:1,
